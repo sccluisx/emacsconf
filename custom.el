72be0beef1d863707f1f5f9de3db2fdc0397b6b8
Luis Enrique Ramirez Chavez <sccluisx@gmail.com>
2016-08-15 17:51:52 +0200

    adding ignorefiles

--- custom.el
+++ custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values
+   (quote
+    ((company-clang-arguments "-I/home/lramirez/emacs_tr/c-demo-project-master/include1/" "-I/home/lramirez/emacs_tr/c-demo-project-master/include2/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "Source Code Pro")))))
+ )
